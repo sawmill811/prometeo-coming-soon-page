f876f364f0bf218b6bf4ae8c7326b2e55575837b
Soumil Sinha <sinha.5@iitj.ac.in>
2022-10-06 10:18:10 +0530

    fixing assets

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 var img = new Image();
-img.src = '/assets/laptop.png';
+img.src = 'prometeo-coming-soon-page/assets/laptop.png';
 img.onload = function() {
     console.log('laptop image loaded');
     document.getElementById('loading').style.display = 'block';
